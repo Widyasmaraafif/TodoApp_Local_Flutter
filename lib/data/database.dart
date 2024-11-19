b56597a5d87d8e2c7552aabbbf143c3490f31bec
Pozzy <widyasmaraanr25@gmail.com>
2024-11-19 22:57:40 +0700

    first commit

---
+++ lib/data/database.dart
@@ -0,0 +1,25 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDatabase {
+  List toDoList = [];
+  //reference our box
+  final _myBox = Hive.box('myBox');
+
+  //run this method if this is the first time ever opening this app
+  void createInitialData() {
+    toDoList = [
+      ["Make Tutorial", false],
+      ["Do Exercise", false],
+    ];
+  }
+
+  //load the data from databbase
+  void loadData() {
+    toDoList = _myBox.get("TODOLIST");
+  }
+
+  //update the database
+  void updateDatabase() {
+    _myBox.put("TODOLIST", toDoList);
+  }
+}
